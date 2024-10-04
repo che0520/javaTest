e642d99846d45d4912f5a54bf55a0b9946ab1c93
che0520 <che0520@mail.green1450.com>
2024-10-04 13:52:10 +0800

    test

---
+++ src/main/java/tw/com/green1450/App.java
@@ -0,0 +1,13 @@
+package tw.com.green1450;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+
+
+// write a hello world class
